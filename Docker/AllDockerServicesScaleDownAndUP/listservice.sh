b9b59e939744dede5cc6bd7bc2e7327eb893f3ac
Reza Darzi <31057674+rezadarzi@users.noreply.github.com>
2023-06-20 10:28:32 +0330

    Delete Docker/AllDockerServicesScaleDownAndUP directory

--- Docker/AllDockerServicesScaleDownAndUP/listservice.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-for i in `docker service ls | grep -v NAME | awk '{print $2}'`
-    do 
-    for j in `docker service ls | grep -v NAME | grep $i |  awk '{print $4}' | cut -d "/" -f 2`
-        do 
-        echo $i $j >> serviceinfo
-    done
-done
--- Docker/AllDockerServicesScaleDownAndUP/scalezero.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-for i in `docker service ls | grep -v NAME | awk '{print $2}'`
-    do docker service scale $i=0
-done
